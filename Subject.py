e63cbb22b22cd190c0403539b47e6269cbca0463
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-07-01 01:14:21 +0330

    added decorator for Subject class

--- Subject.py
+++ Subject.py
@@ -1,3 +1,4 @@
+import MyFunctions
 class Subject(object):
     courses = {}
     def __init__(self, course_code, course_name, course_unit, stu_list, stu_grade):
@@ -6,3 +7,34 @@ class Subject(object):
         self.course_unit = course_unit
         self.stu_list = []
         Subject.courses[course_code] = {course_name: course_unit}
+
+    #==========PROPERTIES AND SETTERS==========
+    @property
+    def course_code(self):
+        return self._course_code
+    @course_code.setter
+    def course_code(self, new_code):
+        if not MyFunctions.is_digits(new_code, 10):
+            raise ValueError("Invalid input. Course code must have only 10 digits. ")
+        self._course_code = new_code
+
+    @property
+    def course_name(self):
+        return self._course_name
+    @course_name.setter
+    def course_name(self, new_name):
+        Subject.courses[self.course_code] = {"course name": new_name}
+        self._course_name = new_name
+
+    @property
+    def course_unit(self):
+        return self._course_unit
+    @course_unit.setter
+    def course_unit(self, new_unit):
+        try:
+            self._course_unit = int(new_unit)
+            Subject.courses[self.course_code] = {"unit": self._course_unit}
+        except ValueError:
+            raise ValueError("Invalid input. Course unit must be an integer.")
+
+    #==========METHODS==========
