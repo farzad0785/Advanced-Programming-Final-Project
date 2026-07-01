60253559e10f69728596fb7b3b112aa9f25fdbb8
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-07-01 16:07:29 +0330

    Added docstrings for MyFunctions class.

--- MyFunctions.py
+++ MyFunctions.py
@@ -1,12 +1,15 @@
 import re
 
 def is_capitalized(text):
+    """Validation. return True if text is capitalized."""
     return bool(text and re.match(r'^[A-Z][a-z]*$', text))
 
 def is_digits(text, amount):
+    """Validation. return True if text has only and exactly 'amount' digits."""
     return bool(text and re.match(rf'^\d{{{amount}}}$', text))
 
 #Not finished yet
+"""Sorter algorithm based on merge sort. Sorts according to the user's choice. """
 def merge_sort():
     pass
 def merge():
