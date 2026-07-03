5fbf223df77c8254bfb07431b87df5ce4baa8b91
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-07-03 23:29:15 +0330

    Adding merge_sort and merge functions to Utils class.

--- Utils.py
+++ Utils.py
@@ -10,7 +10,44 @@ def is_digits(text, amount):
 
 #Not finished yet
 """Sorter algorithm based on merge sort. Sorts according to the user's choice. """
-def merge_sort():
-    pass
-def merge():
-    pass
+def merge_sort(lst, key):
+    if len(lst) < 2:
+        return lst
+    middle = len(lst)//2
+    left = merge_sort(lst[:middle], key)
+    right = merge_sort(lst[middle:], key)
+    return merge(left, right, key)
+
+def merge(left, right, key):
+    result = []
+    i,j = 0,0
+    if key == "gpa":
+        while i < len(left) and j < len(right):
+            if left[i][1].gpa >= right[j][1].gpa:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+    elif key == "first name":
+        while i < len(left) and j < len(right):
+            if left[i][1].f_name >= right[j][1].f_name:
+                result.append(right[j])
+                j += 1
+            else:
+                result.append(left[i])
+                i += 1
+
+    else:
+        while i < len(left) and j < len(right):
+            if left[i][1].l_name >= right[j][1].l_name:
+                result.append(right[j])
+                j += 1
+            else:
+                result.append(left[i])
+                i += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
