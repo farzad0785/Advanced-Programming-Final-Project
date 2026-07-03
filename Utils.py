15e8edcc5e3d0712f1f4fdd0898c6bec3c75d7b8
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-07-03 23:30:15 +0330

    Adding merge_sort and merge functions to Utils class.

--- Utils.py
+++ Utils.py
@@ -8,9 +8,9 @@ def is_digits(text, amount):
     """Validation. return True if text has only and exactly 'amount' digits."""
     return bool(text and re.match(rf'^\d{{{amount}}}$', text))
 
-#Not finished yet
-"""Sorter algorithm based on merge sort. Sorts according to the user's choice. """
+
 def merge_sort(lst, key):
+    """Sorter algorithm based on merge sort. Sorts according to the user's choice. """
     if len(lst) < 2:
         return lst
     middle = len(lst)//2
