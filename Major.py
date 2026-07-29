331e1e6875f8d0c2fab130b4f3847f90d42bbfcd
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-07-29 17:25:51 +0330

    Final debug. Ready to use.

---
+++ Major.py
@@ -0,0 +1,15 @@
+class Major:
+    all_majors = []
+
+    #==========STATIC METHODS==========
+    @staticmethod
+    def add_major(major):
+        if major.capitalize() in Major.all_majors:
+            raise ValueError(f"Invalid input. Major {major} already exist.")
+        Major.all_majors.append(major.capitalize())
+
+    @staticmethod
+    def print_all_majors():
+        print(f"Total majors: {len(Major.all_majors)}")
+        for major in Major.all_majors:
+            print(major)
